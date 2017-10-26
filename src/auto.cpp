572c51374e74c3ebf65d89d7b5f6b5e51c7e6649
Ashwin Gupta <ashwingupta2000@gmail.com>
2017-10-26 16:41:47 -0700

    finished 50 point skillz with lots of delay

--- src/auto.cpp
+++ src/auto.cpp
@@ -41,54 +41,29 @@ int numAuto = 6;
       drive->move(48, 20, -1, 2000);
       drive->move(3, 15, 1, 2000);
       drive->turn(90, 70, -1);
-      drive->move(65, 45, 1, 15, 3000);
+      drive->move(65, 40, 1, 20, 3000);
       mogolift->moveMin();
     //  drive->move(20, 20, -1, 1000);
-      drive->move(12, 45, -1, 3000);
+      drive->move(12, 45, -1, 40, 3000);
       //put down mogo
 
       mogolift->moveMax();
-      drive->move(10, 15, 1, 3000);
+      drive->move(10, 10, 1,  3000);
       drive->move(10, 20, -1, 3000);
       drive->turn(90, 70, 1);
       //drive->move(50, 10, -1, 2000);
       while(digitalRead(C_LIMIT) == true) drive->setDrive(-30, -30);
       drive->setDrive(10, -30);
-      delay(500);
+      delay(1000);
       drive->setDrive(0,0);
-
-
-
-    /*
+      drive->move(38, 15, 1, 3000);
+      drive->turn(45, 70, 1);
       mogolift->moveMin();
-      drive->move(40, 90, 1, 3000); //65
-      drive->move(15, 15, 1, 3000); //65
+      drive->move(64, 15, 1, 3000);
       mogolift->moveMax();
-      drive->move(36, 50, -1, 3000);
-      delay(500);
       drive->turn(90, 70, -1);
-      drive->move(6, 50, -1, 1000);
-      drive->move(2, 50, 1, 2000);
-      //drive->move(5, 127, -1, DRIVE_MIN_SPEED+7, 2000);
-
-      drive->turn(45, 60, -1);
-      //drive->move(6, 127, 1, DRIVE_MIN_SPEED + 7, 1000);
-      //drive->turn(45, 70, 1);
-
-      drive->move(23, 50, 1, 2000);
-      drive->turn(90, 70, -1);
-
-      drive->move(25, 127, 1, 1000);
-      delay(500);
-      drive->move(10, 127, -1, 127, 500);
-      //out of 20
-      delay(500);
-      drive->move(15, 30, 1, 500);
-      delay(500);
-      drive->move(6, 70, -1, 20, 500);
+      drive->move(18, 20, 1, 3000);
       drive->turn(90, 70, 1);
-      drive->move(50, 50, -1, 50, 2000);
-      */
-
+      drive->move(25, 127, 1, 127, 3000);
 
   }
